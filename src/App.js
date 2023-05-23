80c5d008e632259eb84094f6285ec70aa7f08bbe
Arshan <arshannawaz.one@gmail.com>
2023-05-24 01:45:17 +0500

    react-infinite-scroll-component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMoreData, setHasMoreData] = useState(true);
+  const fetchData = () => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=15`)
+      .then((res) => {
+        const newItems = res.data;
+        if (newItems.length > 0) {
+          setItems([...items, ...newItems]);
+          setHasMoreData(true);
+        } else {
+          setHasMoreData(false);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <InfiniteScroll
+      dataLength={items.length} //This is important field to render the next data
+      next={() => {
+        setPage((prevPage) => prevPage + 1);
+      }}
+      hasMore={hasMoreData}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p style={{ textAlign: "center" }}>
+          <b>Yay! You have seen it all</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      }
+    >
+      <div>
+        {items.map((item, index) => (
+          <div key={index}>
+            <h3>
+              {item.id} {item.title}
+            </h3>
+          </div>
+        ))}
+      </div>
+    </InfiniteScroll>
   );
 }
-
-export default App;
